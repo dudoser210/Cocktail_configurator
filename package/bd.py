044110a5f3c071dc97d9b1226b55a8b1c6ec9060
nagibator6k <vanaiox@mail.ru>
2024-05-14 14:40:49 +0300

    Вход

--- package/bd.py
+++ package/bd.py
@@ -16,4 +16,27 @@ def reg(name, password):
         insert_query = f"INSERT INTO userdata (nickname, password) VALUES ('{name}', '{password}');"
         cursor.execute(insert_query)
         mydb.commit()
-c = reg('ututut','3uy3y3y3')
+
+
+def enter(name,password):
+    with mydb.cursor() as cursor:
+        insert_query = f"select password from userdata where nickname = '{name}'"
+        cursor.execute(insert_query)
+        check_pass = cursor.fetchall()
+        mydb.commit()
+        if str(check_pass).split("'")[3] == password:
+            return True
+        else:
+            return False
+
+def create_coctail():
+    pass
+
+def search_ingrideents():
+    pass
+
+def search_coctail_name():
+    pass
+
+def search_coctail_ingredients():
+    pass
